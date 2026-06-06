4423e7cf129e06d2293b3659dc398cd69bde719f
midilli35 <ulvincanbulat@gmail.com>
2026-06-06 19:59:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,67 @@ genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 st.set_page_config(page_title="Smart Compost Coach")
 
-st.title("🌱 Smart Compost Coach")
-st.markdown("### 📷 Compost Snapshot")
+st.markdown("""
+
+<style>
+
+.hero-card{
+    background: linear-gradient(
+        135deg,
+        #FFE9BD,
+        #FFD580
+    );
+    border-radius:20px;
+    padding:25px;
+    margin-bottom:20px;
+}
+
+.hero-title{
+    font-size:42px;
+    font-weight:700;
+    color:#2E2E2E;
+    line-height:1.1;
+}
+
+.hero-subtitle{
+    color:#666666;
+    margin-top:8px;
+    font-size:18px;
+}
+
+.upload-card{
+    background:white;
+    border-radius:50px;
+    padding:15px 25px;
+    border:2px solid #F0F0F0;
+    margin-top:20px;
+}
+
+</style>
+
+""", unsafe_allow_html=True)
+
+st.markdown("""
+
+<div class="hero-card">
+    <div class="hero-title">
+        🌱 Smart Compost Coach
+    </div>
+    <div class="hero-subtitle">
+        Analyze your compost and get instant AI feedback.
+    </div>
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("### 📷 Analyze your compost")
 
 camera_photo = st.camera_input("Take a photo")
 
 uploaded_file = st.file_uploader(
-    "or upload an image",
-    type=["jpg", "jpeg", "png"]
+"",
+type=["jpg", "jpeg", "png"]
 )
+
 
 if camera_photo:
     image_file = camera_photo
